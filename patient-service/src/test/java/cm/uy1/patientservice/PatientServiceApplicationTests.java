534df97fa4e719fe0dfa04d6c7828b5257f9b3a8
benjamin OHANDJA <bendevtech16@gmail.com>
2023-10-15 17:16:51 +0100

    patient-service

---
+++ patient-service/src/test/java/cm/uy1/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.uy1.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
