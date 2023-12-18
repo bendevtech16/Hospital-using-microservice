6197fa8bfbfb4bae8c52ddae18525c3d8dc7a381
benjamin OHANDJA <bendevtech16@gmail.com>
2023-12-18 17:56:15 +0100

    a

---
+++ facturation-service/src/main/java/uy1/inf331/facturationservice/FacturationServiceApplication.java
@@ -0,0 +1,13 @@
+package uy1.inf331.facturationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturationServiceApplication.class, args);
+	}
+
+}
---
+++ facturation-service/src/test/java/uy1/inf331/facturationservice/FacturationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uy1.inf331.facturationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
